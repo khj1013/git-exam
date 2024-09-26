3146813ef3fccb2213c7abf4be676322c946dcc2
heejin <khj1013@skuniv.ac.kr>
2024-09-26 11:09:47 +0900

    modify test03.py by user01

--- test03.py
+++ test03.py
@@ -1,7 +1,9 @@
 #test03.py
 
 def sayHello(name):
-    return 'Hello, ' + name
+    return 'Hello, ' + name + '!!" #user01수정
     
 inName = Tom
 print(sayHello(inName))
+
+print('Requested by boss') #user01추가
